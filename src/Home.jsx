c73755b366f2906be446789a1827e9d73111f28c
amalda1234a <amaldajose2021@gmail.com>
2025-02-24 12:27:45 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Home() {
+  return (
+    <div>
+        <h1>Home Page</h1>
+        <Link to="/About">About us</Link>
+    </div>
+  )
+}
+
+export default Home
